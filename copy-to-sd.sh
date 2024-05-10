e00a9f780079d5bedab4c0964a9e6c4426143d2c
Wannes <wannesdewit@skynet.be>
2024-05-10 19:07:19 +0200

    Copy both kernel images to SD, needs to be tested

--- copy-to-sd.sh
+++ copy-to-sd.sh
@@ -1,5 +1,7 @@
 #!/bin/sh 
 
+# Gets executed inside the buildrtlinux Docker image, so $USER points to the Docker user
+
 cd /home/${USER}/linux/
 
 ## Install kernel modules to SD
@@ -7,9 +9,8 @@ sudo env PATH=$PATH make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- INSTALL_MOD
 
 ## Copy kernel and device Tree blobs to SD
 sudo cp /mnt/fat32/$KERNEL.img /mnt/fat32/$KERNEL-backup.img
-sudo cp arch/arm64/boot/Image /mnt/fat32/$KERNEL.img # TODO check both images are copied to sd card
+sudo cp arch/arm64/boot/Image-v8-16k-stock /mnt/fat32/$KERNEL-v8-16k-stock.img # TODO not tested, check if both images are correctly copied to sd card
+sudo cp arch/arm64/boot/Image-v8-16k-rt /mnt/fat32/$KERNEL-v8-16k-rt.img
 sudo cp arch/arm64/boot/dts/broadcom/*.dtb /mnt/fat32/
 sudo cp arch/arm64/boot/dts/overlays/*.dtb* /mnt/fat32/overlays/
 sudo cp arch/arm64/boot/dts/overlays/README /mnt/fat32/overlays/
-
-# -v8-16k-stock -v8-16k-rt ??
